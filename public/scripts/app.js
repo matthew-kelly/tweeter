6b3d2895433bd03af4d923f9cf86736465f27774
Matt Kelly <bmatthewkelly@gmail.com>
2018-07-18 02:24:40 +0000

    populated feed with tweets from database, added checks for empty tweets or tweets that are too long

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,57 +1,14 @@
 $(document).ready(function () {
-  // Fake data taken from tweets.json
-  const sample = [{
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1531870007000
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1361113796368
-    }
-  ];
 
   $("form").submit(function (event) {
     event.preventDefault();
     let $form = $(this);
     let $tweetText = $form.children("textarea");
     if ($tweetText.val().length === 0) {
+      alert("Tweet area is empty!");
+      return false;
+    } else if ($tweetText.val().length > 140) {
+      alert("Tweet cannot be longer than 140 characters!");
       return false;
     }
     $.ajax({
@@ -103,5 +60,16 @@ $(document).ready(function () {
     return $tweet;
   }
 
-  renderTweets(sample);
+  function loadTweets() {
+    $.ajax({
+      method: "GET",
+      url: "/tweets",
+      dataType: "json",
+      success: function (result) {
+        renderTweets(result);
+      }
+    })
+  }
+
+  loadTweets();
 });
